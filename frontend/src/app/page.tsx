6d4d8d0bb8699df82230fa447285b5d4809fd922
shaheerzkhan <szkhan@sunbonn.com>
2025-06-19 14:52:47 +0500

    initial setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="font-bold text-amber-400">Boiler Plate</div>
+  );
+}
